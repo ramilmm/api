67291fd5170de51e4a009d9343317b14ae0db799
ramilmm <ramilmmk@gmail.com>
2017-06-04 15:42:10 +0300

    fix bug

--- src/main/java/com/example/controller/TodoController.java
+++ src/main/java/com/example/controller/TodoController.java
@@ -14,28 +14,34 @@ public class TodoController {
 	
 	@Autowired
 	private TodoService todoService;
-	
+
+	@CrossOrigin(origins = "*")
     @GetMapping("/api/todo")
 	@ResponseBody
     public List<Todo> getAllTodos() {
 		return todoService.getAllTodos();
 	}
 
+	@CrossOrigin(origins = "*")
 	@PostMapping("/api/todo")
 	public void saveTodo(@Valid @RequestBody Todo todo) { todoService.saveTodo(todo); }
 
+	@CrossOrigin(origins = "*")
 	@PutMapping("/api/todo")
 	public void updateTodo(@Valid @RequestBody Todo todo) { todoService.saveTodo(todo);}
 
+	@CrossOrigin(origins = "*")
     @GetMapping("/api/todo/{id}")
 	@ResponseBody
     public Todo getTodo(@PathVariable("id") long id) {
 		return todoService.getTodoById(id);
 	}
 
+	@CrossOrigin(origins = "*")
 	@DeleteMapping("/api/todo/{id}")
 	public void deleteById(@PathVariable("id") Long id) { todoService.deleteTodo(id); }
 
+	@CrossOrigin(origins = "*")
 	@GetMapping("/api/todo/finished")
 	public List<Todo> getAllFinishedTodos() {
     	return todoService.getFinishedTodos();
